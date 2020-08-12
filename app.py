9f46cbca0477017b4d35a7f763825d11a15cf580
TerryRoberts1995 <terryjbroberts@yahoo.com>
2020-08-12 10:19:46 -0500

    added gitignore

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ from database.db import initialize_db
 from flask_restful import Api
 from resources.routes import initialize_routes
 
-
 app = Flask(__name__)
 app.config.from_envvar('ENV_FILE_LOCATION')
 api = Api(app)
