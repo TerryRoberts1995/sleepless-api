59d41ac1981a8f080a0e91930414c6da6851e40c
TerryRoberts1995 <terryjbroberts@yahoo.com>
2020-08-17 11:10:13 -0500

    db uri added

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from flask_cors import CORS
 from os import environ
 
 app = Flask(__name__)
-CORS(app, supports_credentials=False)
+CORS(app, supports_credentials = True)
 api = Api(app)
 bcrypt = Bcrypt(app)
 jwt = JWTManager(app)
@@ -20,9 +20,8 @@ app.config['JWT_SECRET_KEY'] = environ.get("JWT_SECRET_KEY")
 app.config['JWT_TOKEN_LOCATION'] = ('cookies', 'headers')
 app.config['JWT_ACCESS_COOKIE_PATH'] = '/api/'
 app.config['JWT_COOKIE_CSRF_PROTECT'] = False
-app.config['PROPAGATE_EXCEPTIONS'] = True
 app.config['MONGODB_SETTINGS'] = {
-    'host': 'mongodb://localhost/image-storage'
+    'host': 'mongodb://heroku_9kggfcsn:qlt00fpk712i53u667nrmmchbo@ds115866.mlab.com:15866/heroku_9kggfcsn'
 }
 initialize_db(app)
 initialize_routes(api)
--- resources/image.py
+++ resources/image.py
@@ -6,14 +6,12 @@ from flask_restful import Resource
 # Gallery is going to keep list of images for the gallery.
 class ImagesApi(Resource):
     def get(self):
-        @cross_origin()
         user_id = get_jwt_identity()
         images = Image.objects().to_json()
         return Response(images, mimetype="application/json", status=200)
 
     
     def post(self):
-        @cross_origin()
         body = request.form
         image = Image(**body)
         image.save()
@@ -27,19 +25,18 @@ class ImagesApi(Resource):
 class ImageApi(Resource):
     
     def put(self, id):
-        @cross_origin()
+        
         image = Image.objects.get(id=id)
         body = request.get_json()
         Image.objects.get(id=id).update(**body)
         return '', 200
    
     def delete(self, id):
-        @cross_origin()
+        
         image = Image.objects.get(id=id)
         image.delete()
         return '', 200
 
     def get(self, id):
-        @cross_origin()
         images = Image.objects.get(id=id).to_json()
         return Response(images, mimetype="application/json", status=200)
